17e5b0e3def229b1309fe8741219a5fcd4264fcd
Ji Hyung Geun <93500829+gudrms@users.noreply.github.com>
2026-01-23 10:15:28 +0900

    [level 2] Title: H-Index, Time: 0.86 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int n = citations.length;
+        Arrays.sort(citations);
+        
+        for(int i = 0; i < n;i++){
+            int h = Math.min(citations[i],n-i);
+            answer = Math.max(answer,h);
+        }
+        
+        return answer;
+    }
+}
